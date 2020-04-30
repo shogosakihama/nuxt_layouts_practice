323f1d5dc0ae593b65cb8875da1fbe068bf932e1
Shogo Sakihama <goshalways@gmail.com>
2020-04-30 21:04:33 +0900

    first commit

---
+++ pages/progate.vue
@@ -0,0 +1,455 @@
+<template>
+<body>
+  <div class="header">
+    <div class="header-logo">Progate</div>
+    <div class="header-list">
+      <ul>
+        <li>プログラミングとは</li>
+        <li>学べるレッスン</li>
+        <li>お問い合わせ</li>
+      </ul>
+    </div>
+    <a class="menu-btn" @click="isActive=!isActive">︙</a>
+    <template class="dropdown-table" v-if="isActive">
+      <div style="position:fixed; left:50%; transform: translate(-50%,150%);z-index: 10">
+        <ul class="dropdown-tables">
+          <li>プログラミングとは</li>
+          <li>学べるレッスン</li>
+          <li>お問い合わせ</li>
+        </ul>
+      </div>
+      <div class="dropdown-overlay" @click="isActive = !isActive" v-if="isActive"></div>
+    </template>
+  </div>
+
+  <div class="main">
+    <div class="copy-container">
+      <h1>
+        HELLO WORLD<span>.</span>
+      </h1>
+      <h2>
+        プログラミングの世界へ
+        <br />ようこそ
+      </h2>
+    </div>
+
+    <div class="contents" v-if="!isActive">
+      <h3 class="section-title">学べるレッスン</h3>
+
+      <div class="lessons"></div>
+      <div class="lesson">
+        <div class="lesson-icon">
+          <div class="image-htmlCss"></div>
+          <p>HTML & CSS</p>
+        </div>
+        <!-- <p class="text-contents">HTML & CSS</p> -->
+      </div>
+      <div class="lesson">
+        <div class="lesson-icon">
+          <div class="image-jQuery"></div>
+          <p>jQuery</p>
+        </div>
+        <!-- <p class="text-contents">jQuery</p> -->
+      </div>
+      <div class="lesson">
+        <div class="lesson-icon">
+          <div class="image-ruby"></div>
+          <p>Ruby</p>
+        </div>
+        <!-- <p class="text-contents">Ruby</p> -->
+      </div>
+      <div class="lesson">
+        <div class="lesson-icon">
+          <div class="image-php"></div>
+          <p>PHP</p>
+        </div>
+        <!-- <p class="text-contents">PHP</p> -->
+      </div>
+    </div>
+
+    <!-- <div class="contents-item">
+        <div class="image_htmlCss"></div>
+        <p>HTML & CSS</p>
+      </div>
+      <div class="contents-item">
+        <div class="image_php"></div>
+        <p>PHP</p>
+      </div>
+      <br>
+      <div class="contents-item">
+        <div class="image_ruby"></div>
+        <p>Ruby</p>
+      </div>
+      <div class="contents-item">
+        <div class="image_swift"></div>
+        <p>Swift</p>
+    </div>-->
+    <!-- </div> -->
+
+    <div class="contact-form">
+      <h3 class="section-title">お問い合わせ</h3>
+      <p>メールアドレス（必須）</p>
+      <input />
+      <p>お問い合わせ内容（必須）</p>
+      <textarea></textarea>
+      <p>※必須項目は必ずご入力ください</p>
+      <input class="contact-submit" type="submit" value="送信" />
+    </div>
+  </div>
+
+  <div class="footer">
+    <div class="footer-logo">Progate</div>
+    <div class="footer-list"  v-if="!isActive">
+      <ul>
+        <li>会社概要</li>
+        <li>採用</li>
+        <li>お問い合わせ</li>
+      </ul>
+    </div>
+  </div>
+</body>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      isActive: false
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+@mixin box-size3 {
+  padding: 110px;
+  margin-right: 10px;
+  @content;
+}
+
+@include media(xl) {
+  .header {
+    background-color: #26d0c9;
+    color: #fff;
+    height: 90px;
+    width: 100%;
+    .header-logo {
+      float: left;
+      font-size: 36px;
+      padding: 20px 40px;
+    }
+    .header-list {
+      float: right;
+      li {
+        float: left;
+        padding: 33px 20px;
+      }
+    }
+    .menu-btn {
+      display: none;
+    }
+    .dropdown-tables {
+      display: none;
+    }
+  }
+
+  .main {
+    padding: 100px 4%;
+    .copy-container {
+      h1 {
+        font-size: 140px;
+        span {
+          color: #ff4a4a;
+        }
+      }
+
+      h2 {
+        font-size: 60px;
+      }
+    }
+      .contents {
+        width: 100%;
+        height: 500px;
+        margin-top: 100px;
+        overflow-x: auto;
+        .section-title {
+          border-bottom: 2px solid #dee7ec;
+          font-size: 28px;
+          padding-bottom: 15px;
+          margin-bottom: 50px;
+        }
+
+      $box-size: 120px;
+
+      .lesson {
+        float: left;
+        width: 25%;
+        padding: 0 4%;
+        .lesson-icon {
+          position: relative;
+          .image-htmlCss {
+            transform: rotateZ(45deg);
+            padding: $box-size;
+            background-color: #ff4b4b;
+          }
+          .image-jQuery {
+            transform: rotateZ(45deg);
+            padding: $box-size;
+            background-color: #9f4bff;
+          }
+          .image-ruby {
+            transform: rotateZ(45deg);
+            padding: $box-size;
+            background-color: #14c8ff;
+          }
+          .image-php {
+            transform: rotateZ(45deg);
+            padding: $box-size;
+            background-color: #4bff69;
+          }
+        }
+      }
+      .lesson-icon p {
+        position: absolute;
+        top: 43%;
+        width: 100%;
+        color: white;
+        font-size: 150%;
+      }
+      .text-contents {
+        width: 80%;
+        display: inline-block;
+        margin-top: 45px;
+        font-size: 23px;
+        color: #4e4e4e;
+      }
+    }
+    
+    .contact-form {
+      padding-top: 100px;
+      .section-title {
+        border-bottom: 2px solid #dee7ec;
+        font-size: 28px;
+        padding-bottom: 15px;
+        margin-bottom: 50px;
+      }
+    }
+    input,
+    textarea {
+      width: 400px;
+      margin-top: 10px;
+      margin-bottom: 30px;
+      padding: 20px;
+      font-size: 18px;
+      border: 1px solid #dee7ec;
+    }
+
+    .contact-submit {
+      background-color: #dee7ec;
+      color: #889eab;
+    }
+  }
+  .footer {
+    background-color: #2f5167;
+    color: #fff;
+    height: 120px;
+    padding: 40px;
+    .footer-logo {
+      float: left;
+      font-size: 32px;
+    }
+    .footer-list {
+      float: right;
+      li {
+        margin-bottom: 0%;
+        transform: translate(0, -50%);
+      }
+    }
+  }
+}
+
+@mixin box-size2 {
+  padding: 130px;
+  @content;
+}
+
+@include media(s) {
+  .header {
+    background-color: #26d0c9;
+    color: #fff;
+    height: 70px;
+    .header-logo {
+      float: left;
+      font-size: 30px;
+      padding: 4% 7% 4% 7%;
+      font-weight: 500;
+    }
+    .header-list {
+      display: none;
+      float: right;
+      li {
+        float: left;
+        padding: 33px 20px;
+      }
+    }
+    .menu-btn {
+      display: block;
+      float: right;
+      padding: 5%;
+      transform: translate(0, -10%);
+      font-weight: bold;
+      font-size: 200%;
+    }
+    .dropdown-tables {
+      display: block;
+      z-index: 999;
+      background-color: #26d0c9;
+      padding: 20px;
+      width: 160%;
+      border-radius: 10px;
+      text-align: center;
+      margin-right: 0;
+      transform: translate(-18.5%, -40%);
+      li {
+        color: #000000;
+        background-color: #ffffff;
+        z-index: 8;
+        padding: 15px;
+        font-size: 120%;
+        font-weight: bold;
+      }
+      li:hover {
+        background-color: #daffe1;
+      }
+    }
+    .dropdown-overlay {
+      z-index: 0;
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 120%;
+      background-color: rgba(0, 0, 0, 0.75);
+    }
+  }
+
+  .main {
+    all: initial;
+    text-align: center;
+    .copy-container {
+      h1 {
+        text-align: left;
+        font-size: 435%;
+        padding: 10% 2% 10% 6%;
+        span {
+          color: #ff4a4a;
+        }
+      }
+
+      h2 {
+        font-size: 170%;
+        font-weight: normal;
+      }
+    }
+    .contents {
+      margin-top: 120px;
+      overflow-x: visible;
+      .section-title {
+        border-bottom: 2px solid #dee7ec;
+        font-size: 28px;
+        padding-bottom: 15px;
+        margin-bottom: 100px;
+      }
+      $box-size: 52%;
+
+      .lesson {
+        // display: block;
+        width: 52%;
+        padding: 5% 0%;
+        transform: scale(1.2);
+        margin-left: 22%;
+        .lesson-icon {
+          position: relative;
+          .image-htmlCss {
+            transform: rotateZ(45deg);
+            padding: $box-size;
+          }
+          .image-jQuery {
+            transform: rotateZ(45deg);
+            padding: $box-size;
+            background-color: #9f4bff;
+          }
+          .image-ruby {
+            transform: rotateZ(45deg);
+            padding: $box-size;
+            background-color: #14c8ff;
+          }
+          .image-php {
+            transform: rotateZ(45deg);
+            padding: $box-size;
+            background-color: #4bff69;
+          }
+        }
+      }
+      .lesson-icon p {
+        position: absolute;
+        top: 44%;
+        width: 100%;
+        color: white;
+        font-size: 100%;
+      }
+      .text-contents {
+        width: 80%;
+        display: inline-block;
+        margin-top: 45px;
+        font-size: 23px;
+        color: #4e4e4e;
+      }
+    }
+
+    .contact-form {
+      padding-top: 550px;
+      .section-title {
+        border-bottom: 2px solid #dee7ec;
+        font-size: 28px;
+        padding-top: 90px;
+        padding-bottom: 15px;
+        margin-bottom: 50px;
+      }
+    }
+    input,
+    textarea {
+      width: 250px;
+      margin-top: 10px;
+      margin-bottom: 30px;
+      padding: 20px;
+      font-size: 10px;
+      border: 1px solid #dee7ec;
+    }
+
+    .contact-submit {
+      background-color: #dee7ec;
+      color: #889eab;
+    }
+  }
+
+  .footer {
+    background-color: #2f5167;
+    color: #fff;
+    height: 120px;
+    padding: 40px;
+    .footer-logo {
+      float: left;
+      font-size: 32px;
+    }
+    .footer-list {
+      float: right;
+      li {
+        margin-bottom: 0%;
+        transform: translate(0, -50%, 0);
+        // color:black;
+      }
+    }
+  }
+}
+</style>
---
+++ pages/progate2.vue
@@ -0,0 +1,612 @@
+<template>
+<body>
+  <header>
+    <div class="container">
+      <div class="header-left">Progate</div>
+      <div class="header-right">
+        <a class="login" @click="isActive=!isActive">ログイン</a>
+        <template class="dropdown" v-if="isActive">
+          <div
+            class="dropdown-tables"
+            style="position: fixed;left:50%;transform: translate(-50%,20%);"
+          >
+            <h3>アカウントを作成</h3>
+            <input placeholder="ユーザー名" />
+            <input placeholder="メールアドレス" />
+            <input placeholder="パスワード" />
+            <input class="contact-submit" type="submit" />
+            <div class="social-login">
+              <div class="border-or">
+                <div class="border">
+                  <span class="or">または</span>
+                </div>
+              </div>
+              <div class="btn-wrapper">
+                <a href="#" class="btn facebook" style="width: 100%">
+                  <span class="fab fa-facebook-f"></span>Facebookで登録
+                </a>
+                <a href="#" class="btn twitter" style="width: 100%">
+                  <span class="fab fa-twitter"></span>Twitterで登録
+                </a>
+              </div>
+
+              <!-- <div class="google"></div> -->
+              <!-- <div class="facebook">
+                <span class="fab fa-facebook-f"></span>
+              </div>
+              <div class="twitter">
+                <span class="fab fa-twitter"></span>
+              </div>-->
+            </div>
+          </div>
+        </template>
+        <div class="dropdown-overlay" @click="isActive = !isActive" v-if="isActive"></div>
+      </div>
+    </div>
+  </header>
+  <div class="top-wrapper">
+    <div class="container">
+      <h1>
+        LEARN TO CODE.
+        <br />LEARN TO BE CREATIVE.
+      </h1>
+      <p class="sub-intro">
+        Progateはオンラインプログラミング学習サービスです。
+        <br />初心者にもやさしいスライドとレッスンで、ウェブサービスを作りながら学んでいきましょう。
+      </p>
+      <a href="#" class="btn signup">新規登録はこちら</a>
+      <p>or</p>
+      <a href="#" class="btn facebook">
+        <span class="fab fa-facebook-f"></span>Facebookで登録
+      </a>
+      <a href="#" class="btn twitter">
+        <span class="fab fa-twitter"></span>Twitterで登録
+      </a>
+    </div>
+  </div>
+
+  <div class="lesson-wrapper">
+    <div class="container">
+      <div class="heading">
+        <h2>Learn Where to Get Started!</h2>
+      </div>
+
+      <div class="lessons">
+        <div class="lesson">
+          <div class="lesson-icon">
+            <div class="image-htmlCss"></div>
+            <p>HTML & CSS</p>
+          </div>
+          <p class="text-contents">ウェブページの作成に使用される言語です。HTMLとCSSを組み合わせることで、静的なページを作り上げることができます。</p>
+        </div>
+        <div class="lesson">
+          <div class="lesson-icon">
+            <div class="image-jQuery"></div>
+            <p>jQuery</p>
+          </div>
+          <p
+            class="text-contents"
+          >素敵な動きを手軽に実装できるJavaScriptライブラリです。 アニメーション効果をつけたり、Ajax（エイジャックス）を使って外部ファイルを読み込んだりと色々なことができます。</p>
+        </div>
+        <div class="lesson">
+          <div class="lesson-icon">
+            <div class="image-ruby"></div>
+            <p>Ruby</p>
+          </div>
+          <p
+            class="text-contents"
+          >オープンソースの動的なプログラミング言語で、 シンプルさと高い生産性を備えています。大きなWebアプリケーションから小さな日用ツールまで、さまざまなソフトウェアを作ることができます。</p>
+        </div>
+        <div class="lesson">
+          <div class="lesson-icon">
+            <div class="image-php"></div>
+            <p>PHP</p>
+          </div>
+          <p
+            class="text-contents"
+          >オープンソースの動的なプログラミング言語で、 シンプルさと高い生産性を備えています。大きなWebアプリケーションから小さな日用ツールまで、さまざまなソフトウェアを作ることができます。</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="message-wrapper">
+    <div class="container">
+      <div class="heading">
+        <h2>さぁ、あなたもProgateでプログラミングを学んでみませんか?</h2>
+        <h3>Let's learn to code, learn to be creative!</h3>
+      </div>
+      <span class="btn message">さっそく開発する</span>
+    </div>
+  </div>
+
+  <footer>
+    <div class="container">
+      <div class="footer-left">Progate</div>
+      <p>Learn to Code, Learn to be Creative.</p>
+    </div>
+  </footer>
+</body>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      isActive: false
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+@include media(xl) {
+  .top-wrapper {
+    padding: 280px 0 280px 0;
+    background-image: url("~assets/work-731198_1920.jpg");
+    background-size: cover;
+    color: white;
+    text-align: center;
+    h1 {
+      opacity: 0.7;
+      font-size: 45px;
+      letter-spacing: 5px;
+    }
+    p {
+      opacity: 0.7;
+      margin-bottom: 15px;
+    }
+    .container {
+      width: 100%;
+      margin: 0 auto;
+      .btn {
+        padding: 8px 24px;
+        color: white;
+        display: inline-block;
+        opacity: 0.8;
+        border-radius: 4px;
+      }
+      .btn:hover {
+        opacity: 1;
+      }
+      .fab {
+        margin-right: 5px;
+      }
+      .signup {
+        background-color: #239b76;
+        margin-top: 15px;
+        margin-bottom: 15px;
+      }
+      .facebook {
+        background-color: #3b5998;
+        margin-right: 10px;
+      }
+      .twitter {
+        background-color: #55acee;
+      }
+    }
+  }
+
+  header {
+    height: 65px;
+    width: 100%;
+    background-color: rgba(34, 49, 52, 0.9);
+    position: fixed;
+    top: 0;
+    z-index: 10;
+    .dropdown-overlay {
+      z-index: 0;
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 120%;
+      background-color: rgba(0, 0, 0, 0.75);
+    }
+    .logo {
+      width: 124px;
+      margin-top: 20px;
+    }
+    .header-left {
+      float: left;
+      color: rgba(255, 255, 255, 0.9);
+      font-size: 200%;
+      padding: 10px 10px 20px 20px;
+    }
+    .header-right {
+      float: right;
+      background-color: rgba(255, 255, 255, 0.3);
+      transition: all 0.5s;
+    }
+    .header-right:hover {
+      background-color: rgba(255, 255, 255, 0.5);
+    }
+    .header-right a {
+      line-height: 65px;
+      padding: 0 25px;
+      color: white;
+      display: block;
+    }
+    .dropdown-tables {
+      color: #000000;
+      background-color: #ffffff;
+      z-index: 8;
+      font-size: 120%;
+      text-align: center;
+      border-radius: 8px;
+      width: 40%;
+      padding: 2% 0;
+    }
+    input {
+      width: 70%;
+      margin-top: 10px;
+      margin-bottom: 10px;
+      padding: 3%;
+      font-size: 20px;
+      border: 1px solid #dee7ec;
+      border-radius: 4px;
+      color: #889eab;
+    }
+    .contact-submit {
+      background-color: #dee7ec;
+      color: #889eab;
+    }
+    .social-login {
+      .border-or {
+        position: relative;
+        .border {
+          border-top: 1px solid rgba(168, 168, 168, 0.5);
+          padding: 5px;
+          width: 70%;
+          margin-left: auto;
+          margin-right: auto;
+          margin-top: 4%;
+          margin-bottom: 4%;
+        }
+        .or {
+          position: absolute;
+          bottom: 0.2%;
+          left: 42%;
+          right: 42%;
+          @include media(m) {
+            left: 25%;
+            right: 25%;
+          }
+          background-color: #ffffff;
+          padding: 0 5px;
+          font-size: 80%;
+        }
+      }
+      .btn-wrapper {
+        width: 70%;
+      margin-left: auto;
+      margin-right: auto;
+      font-size: 106%;
+        .btn {
+          line-height: 30%;
+          padding: 4% 24px;
+          margin-bottom: 2%;
+          color: white;
+          display: inline-block;
+          opacity: 0.8;
+          border-radius: 4px;
+        }
+        .btn:hover {
+          opacity: 1;
+        }
+        .fab {
+          margin-right: 5px;
+        }
+        // .signup {
+        //   background-color: #239b76;
+        //   margin-top: 15px;
+        //   margin-bottom: 15px;
+        // }
+        .facebook {
+          background-color: #3b5998;
+          margin-right: 10px;
+        }
+        .twitter {
+          background-color: #55acee;
+        }
+      }
+    }
+  }
+
+  .lesson-wrapper {
+    height: 580px;
+    background-color: #f7f7f7;
+    text-align: center;
+    .heading {
+      padding-top: 80px;
+      padding-bottom: 50px;
+      color: #5f5d60;
+    }
+
+    $box-size: 50%;
+
+    .lesson {
+      float: left;
+      width: 25%;
+      padding: 0 3%;
+      .lesson-icon {
+        position: relative;
+        p {
+          font-size: 200%;
+          transform: translate(0, -10%);
+        }
+        .image-htmlCss {
+          padding: $box-size;
+          background-color: #ff4b4b;
+        }
+        .image-jQuery {
+          padding: $box-size;
+          background-color: #9f4bff;
+        }
+        .image-ruby {
+          padding: $box-size;
+          background-color: #14c8ff;
+        }
+        .image-php {
+          padding: $box-size;
+          background-color: #4bff69;
+        }
+      }
+    }
+    .lesson-icon p {
+      position: absolute;
+      top: 44%;
+      width: 100%;
+      color: white;
+    }
+    .text-contents {
+      width: 80%;
+      display: inline-block;
+      margin-top: 15px;
+      font-size: 13px;
+      color: #4e4e4e;
+    }
+  }
+
+  .heading h3 {
+    font-weight: normal;
+  }
+  .message-wrapper {
+    padding-bottom: 80px;
+    text-align: center;
+    .heading {
+      padding-top: 80px;
+      padding-bottom: 50px;
+      color: #5f5d60;
+    }
+    .message {
+      padding: 15px 40px;
+      background-color: #5dca88;
+      cursor: pointer;
+      box-shadow: 0 7px #1a7940;
+    }
+    .message:active {
+      position: relative;
+      top: 7px;
+      box-shadow: none;
+    }
+  }
+
+  footer {
+    padding: 30px 20px;
+    border-top: 1px solid #eee;
+    footer p {
+      color: #b3aeb5;
+      font-size: 12px;
+    }
+    .footer-left {
+      color: rgba(124, 253, 124, 0.9);
+      font-size: 200%;
+    }
+  }
+}
+
+@include media(s) {
+  .top-wrapper {
+    padding: 140px 0 140px 0;
+    background-image: url("~assets/work-731198_1920.jpg");
+    background-size: cover;
+    color: white;
+    text-align: center;
+    h1 {
+      opacity: 0.7;
+      font-size: 45px;
+      letter-spacing: 5px;
+      padding-bottom: 80px;
+    }
+    .sub-intro {
+      padding-bottom: 40px;
+      font-weight: 500;
+    }
+    p {
+      opacity: 0.7;
+      margin-bottom: 15px;
+    }
+    .container {
+      width: 90%;
+      margin: 0 auto;
+      .btn {
+        width: 100%;
+        color: white;
+        display: inline-block;
+        opacity: 0.8;
+        border-radius: 4px;
+        font-size: 110%;
+      }
+      .btn:hover {
+        opacity: 1;
+      }
+      .fab {
+        margin-right: 5px;
+      }
+      .signup {
+        background-color: #239b76;
+        margin-top: 15px;
+        margin-bottom: 15px;
+      }
+      .facebook {
+        background-color: #3b5998;
+        margin-bottom: 15px;
+      }
+      .twitter {
+        background-color: #55acee;
+      }
+    }
+  }
+
+  header {
+    height: 65px;
+    width: 100%;
+    background-color: rgba(34, 49, 52, 0.9);
+    position: fixed;
+    top: 0;
+    z-index: 10;
+    .logo {
+      width: 124px;
+      margin-top: 20px;
+    }
+    .header-left {
+      float: left;
+      color: rgba(255, 255, 255, 0.9);
+      font-size: 200%;
+      padding: 10px 10px 20px 20px;
+    }
+    .header-right {
+      float: right;
+      background-color: rgba(255, 255, 255, 0.3);
+      transition: all 0.5s;
+    }
+    .dropdown-tables {
+      color: #000000;
+      background-color: #ffffff;
+      z-index: 8;
+      font-size: 120%;
+      text-align: center;
+      border-radius: 8px;
+      width: 90%;
+      padding: 2% 0;
+    }
+    .header-right:hover {
+      background-color: rgba(255, 255, 255, 0.5);
+    }
+    .header-right a {
+      line-height: 65px;
+      padding: 0 25px;
+      color: white;
+      display: block;
+    }
+    .btn-wrapper {
+      width: 70%;
+      margin-left: auto;
+      margin-right: auto;
+      font-size: 106%;
+      .btn {
+        padding: 4% 0;
+        margin-bottom: 2%;
+      }
+    }
+  }
+
+  .lesson-wrapper {
+    height: 2170px;
+    background-color: #f7f7f7;
+    text-align: center;
+    .heading {
+      padding-top: 80px;
+      padding-bottom: 80px;
+      color: #5f5d60;
+    }
+    .lessons {
+      display: block;
+      // all: initial;
+    }
+
+    $box-height: 30%;
+
+    .lesson {
+      display: block;
+      width: 100%;
+      padding: 0 5%;
+      .lesson-icon {
+        position: relative;
+        p {
+          font-size: 250%;
+          transform: translate(0, -35%);
+        }
+        .image-htmlCss {
+          padding: $box-height;
+          background-color: #ff4b4b;
+        }
+        .image-jQuery {
+          padding: $box-height;
+          background-color: #9f4bff;
+        }
+        .image-ruby {
+          padding: $box-height;
+          background-color: #14c8ff;
+        }
+        .image-php {
+          padding: $box-height;
+          background-color: #4bff69;
+        }
+      }
+    }
+    .lesson-icon p {
+      position: absolute;
+      top: 44%;
+      width: 100%;
+      color: white;
+    }
+    .text-contents {
+      width: 80%;
+      margin-top: 10px;
+      margin-bottom: 85px;
+      font-size: 20px;
+      color: #4e4e4e;
+    }
+  }
+
+  .heading h3 {
+    font-weight: normal;
+  }
+  .message-wrapper {
+    padding-bottom: 80px;
+    text-align: center;
+    .heading {
+      padding: 80px 20px 50px 20px;
+      padding-bottom: 50px;
+
+      color: #5f5d60;
+    }
+    .message {
+      padding: 15px 40px;
+      background-color: #5dca88;
+      cursor: pointer;
+      box-shadow: 0 7px #1a7940;
+    }
+    .message:active {
+      position: relative;
+      top: 7px;
+      box-shadow: none;
+    }
+  }
+
+  footer {
+    padding: 30px 20px;
+    border-top: 1px solid #eee;
+    footer p {
+      color: #b3aeb5;
+      font-size: 12px;
+    }
+    .footer-left {
+      color: rgba(124, 253, 124, 0.9);
+      font-size: 200%;
+    }
+  }
+}
+</style>
